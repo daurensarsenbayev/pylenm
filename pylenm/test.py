1d9cfdc073bd38dde747d3457a50851398976bdf
Dauren Sarsenbayev <daurensarsenbayev@gmail.com>
2024-05-27 01:25:44 -0400

    added test file

---
+++ pylenm/test.py
@@ -0,0 +1,22 @@
+from pylenm import *
+import pandas as pd
+
+#my_data_url_1 = '../content/sample_data/2018-2023.csv'
+#my_data_url_2 = '../content/sample_data/All_BHs.csv'
+
+url_1 = '../notebooks/data/FASB_Data_thru_3Q2015_Reduced_Demo.csv'
+url_2 = '../notebooks/data/FASB Well Construction Info.xlsx'
+
+concentration_data = pd.read_csv(url_1)
+construction_data = pd.read_excel(url_2)
+
+#After importing conc data, enforce STATION_ID column as a str type, becuase pd infers types
+concentration_data['STATION_ID'] = concentration_data['STATION_ID'].astype(str)
+
+pylenm_df = PylenmDataFactory(concentration_data) # Save concentration data
+pylenm_df.setConstructionData(construction_data) # Save construction data
+
+analytes = ['TRITIUM','SPECIFIC CONDUCTANCE', 'PH','URANIUM-238', 'DEPTH_TO_WATER']
+#pylenm_df.plot_PCA_by_date('1993-02-21', analytes)
+
+pylenm_df.plot_PCA_by_date('1993-02-21', analytes)
